4bed00b7ed9d5de3a47a007c7841b53ab1128f26
Sudhir Voleti <sudhir_voleti@isb.edu>
2026-03-22 08:34:01 +0530

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,5 +1,7 @@
-#src/core/__init__.py:
-"""Core SMC algorithms (forward filtering, tempering)."""
+"""
+Core SMC algorithms (forward filtering, tempering).
+Path: src/core/__init__.py
+"""
 from .forward_filter import forward_filter_log_batched, forward_filter_numpy
 from .tempering import linear_schedule, exponential_schedule, compute_ess
 
